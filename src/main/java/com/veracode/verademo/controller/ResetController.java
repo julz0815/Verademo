78b351f90087295fe167caa819a663345bbce469
jtotzekhallhuber <jtotzekhallhuber@192.168.1.123>
2019-03-04 19:24:19 +1100

    commit

--- src/main/java/com/veracode/verademo/controller/ResetController.java
+++ src/main/java/com/veracode/verademo/controller/ResetController.java
@@ -90,7 +90,7 @@ public class ResetController {
 
 		Random rand = new Random();
 
-		// Drop existing tables and recreate from schema file asfdadsf
+		// Drop existing tables and recreate from schema file
 		recreateDatabaseSchema();
 
 		try {
