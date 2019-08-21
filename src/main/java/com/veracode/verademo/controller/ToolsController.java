afe25d0fbd87b3d480f5fdd1f05cfe8ab868311f
jtotzekhallhuber <jtotzekhallhuber@192.168.178.124>
2019-08-21 15:41:02 +0200

    commit

--- src/main/java/com/veracode/verademo/controller/ToolsController.java
+++ src/main/java/com/veracode/verademo/controller/ToolsController.java
@@ -51,7 +51,7 @@ public class ToolsController {
 		String output = "";
 		Process proc;
 		try {
-			/* START BAD CODE ---*/
+			/* START BAD CODE */
 			if (System.getProperty("os.name").startsWith("Windows")) {
 				proc = Runtime.getRuntime().exec("cmd.exe /c ping " + host);
 			}
